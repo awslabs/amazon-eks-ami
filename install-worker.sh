7c71d2ad7e7dd89083c745e59ce193663f5c1651
Marcelo Bartsch <ruhlem@amazon.com>
2018-07-13 09:48:51 +0100

    Squashed commit of the following:
    commit 1ea10a7d6f6b41e6757f9d16754e1164a68c9c1d
    Author: Marcelo Bartsch <ruhlem@amazon.com>
    Date:   Fri Jul 13 09:21:06 2018 +0100
    
        reset to original files
    
    commit 2aa71175cfe4403f119296c11111723023ed2863
    Author: Marcelo Bartsch <marcelo@bartsch.cl>
    Date:   Thu Jul 12 21:28:12 2018 +0100
    
        make the enable amazon-linux-extra and enable docker a two step
        operation
    
    commit 75805b09d72a002ec9d666bd09836b656ababc75
    Author: Marcelo Bartsch <ruhlem@amazon.com>
    Date:   Wed Jul 11 11:16:52 2018 +0100
    
        added source_ami_id as a empty variable

--- install-worker.sh
+++ install-worker.sh
@@ -16,7 +16,6 @@ sudo yum update -y
 sudo yum install -y \
     aws-cfn-bootstrap \
     conntrack \
-    nfs-utils \
     curl \
     socat \
     unzip \
@@ -45,7 +44,7 @@ sudo systemctl enable iptables-restore
 ################################################################################
 
 sudo yum install -y yum-utils device-mapper-persistent-data lvm2
-sudo amazon-linux-extras install -y docker
+sudo yum install -y docker
 sudo usermod -aG docker $USER
 
 # Enable docker daemon to start on boot.
