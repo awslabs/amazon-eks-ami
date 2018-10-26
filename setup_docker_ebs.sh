e2f9c52f5fa311ff0d2685b13a5999b7e671e3df
Albert Dixon <albert@dixon.rocks>
2018-10-26 14:16:08 -0700

    Patches (#7)
    * updates to make the ami our own
    
    * Merge branch 'master' of github.com:lumoslabs/amazon-eks-ami
    
    * - removed mounting from install-worker.sh
    
    * updated to include label and system.d annotation
    
    * use ext4 instead of xfs
    
    * go back to xfs

--- setup_docker_ebs.sh
+++ setup_docker_ebs.sh
@@ -1,7 +1,26 @@
 #!/usr/bin/env bash
 
 # Provision and mount EBS volume to /var/lib/docker
-sudo mkfs -t xfs -n ftype=1 -L DOCKER $DOCKER_EBS_NAME
+case "$FSTYPE" in
+  xfs) sudo mkfs.xfs -n ftype=1 -i size=512 -L DOCKER $DOCKER_EBS_NAME ;;
+  ext4) sudo mkfs.ext4 -L DOCKER $DOCKER_EBS_NAME ;;
+  *) echo "FSTYPE is required!" >&2 ; exit 1 ;;
+esac
+
 sudo mkdir -vp /var/lib/docker
 sudo mount $DOCKER_EBS_NAME /var/lib/docker
-sudo bash -c "echo -e '$DOCKER_EBS_NAME\t/var/lib/docker\txfs\tdefaults,nofail,x-systemd.before=docker.service\t0\t2' >> /etc/fstab"
+
+{
+  echo -n    "$DOCKER_EBS_NAME"
+  echo -n -e "\t"
+  echo -n    "/var/lib/docker"
+  echo -n -e "\t"
+  echo -n    "$FSTYPE"
+  echo -n -e "\t"
+  echo -n    "defaults,nofail,nodiratime,x-systemd.before=docker.service"
+  echo -n -e "\t"
+  echo -n    "0"
+  echo -n -e "\t"
+  echo -n    "2"
+  echo
+} | sudo tee -a /etc/fstab
