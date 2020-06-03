707fc575a64df66b449a43a1e09cc618d6221370
Bronson Mirafuentes <mirafb@amazon.com>
2020-06-03 09:39:05 -0700

    update logrotate.conf to compress rotated logs (#479)

--- scripts/install-worker.sh
+++ scripts/install-worker.sh
@@ -134,6 +134,7 @@ fi
 # kubelet uses journald which has built-in rotation and capped size.
 # See man 5 journald.conf
 sudo mv $TEMPLATE_DIR/logrotate-kube-proxy /etc/logrotate.d/kube-proxy
+sudo mv $TEMPLATE_DIR/logrotate.conf /etc/logrotate.conf
 sudo chown root:root /etc/logrotate.d/kube-proxy
 sudo mkdir -p /var/log/journal
 
