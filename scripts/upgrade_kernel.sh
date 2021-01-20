abdadef492ddf4ee6fc2bb327ef867eee4257d3b
Matt Merkes <merkes@amazon.com>
2021-01-20 10:18:27 -0800

    Revert "Sets the 5.4 linux kernel as default for kubernetes version 1.19 and higher"
    This reverts commit d028149da221a3de351da2a057d0e4967467a55d.

--- scripts/upgrade_kernel.sh
+++ scripts/upgrade_kernel.sh
@@ -4,27 +4,5 @@ set -o pipefail
 set -o nounset
 set -o errexit
 
-if [[ -z "$KERNEL_VERSION" ]]; then
-    # Makes 5.4 kernel the default on 1.19 and higher
-    IFS='.'
-    # Convert kubernetes version in an array to compare versions
-    read -ra ADDR <<< "$KUBERNETES_VERSION"
-    if (( ADDR[0] == 1 && ADDR[1] < 19 )); then
-        KERNEL_VERSION=4.14
-    else
-        KERNEL_VERSION=5.4
-    fi
-
-    echo "kernel_version is unset. Setting to $KERNEL_VERSION based on kubernetes_version $KUBERNETES_VERSION"
-fi
-
-if [[ $KERNEL_VERSION == "4.14" ]]; then
-    sudo yum update -y kernel
-elif [[ $KERNEL_VERSION == "5.4" ]]; then
-    sudo amazon-linux-extras install -y kernel-5.4
-else
-    echo "$KERNEL_VERSION is not a valid kernel version"
-    exit 1
-fi
-
+sudo yum update -y kernel
 sudo reboot
