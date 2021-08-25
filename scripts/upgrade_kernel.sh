969068587d384c93afb2ea389f36677de2c06470
tuannvm <me@tuannvm.com>
2021-08-25 16:50:48 +0800

    Upstream update

--- scripts/upgrade_kernel.sh
+++ scripts/upgrade_kernel.sh
@@ -17,7 +17,7 @@ if [[ -z "$KERNEL_VERSION" ]]; then
     if (( ADDR[0] == 1 && ADDR[1] < 19 )); then
         KERNEL_VERSION=4.14
     else
-        KERNEL_VERSION=5.4
+        KERNEL_VERSION=5.8
     fi
 
     echo "kernel_version is unset. Setting to $KERNEL_VERSION based on kubernetes_version $KUBERNETES_VERSION"
@@ -26,7 +26,9 @@ fi
 if [[ $KERNEL_VERSION == "4.14" ]]; then
     sudo yum update -y kernel
 elif [[ $KERNEL_VERSION == "5.4" ]]; then
-    sudo amazon-linux-extras install -y kernel-5.4
+    sudo amazon-linux-extras install -y kernel-5.4 kernel-devel
+elif [[ $KERNEL_VERSION == "5.8" ]]; then
+    sudo amazon-linux-extras install -y kernel-5.8
 else
     echo "$KERNEL_VERSION is not a valid kernel version"
     exit 1
