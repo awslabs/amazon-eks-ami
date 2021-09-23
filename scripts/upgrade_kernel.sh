c4d08d2395987da3172c299fa77c52760e3e57e3
Matt <merkes@amazon.com>
2021-09-23 13:55:40 -0700

    Adds option to upgrade to 5.10 kernel (#765)

--- scripts/upgrade_kernel.sh
+++ scripts/upgrade_kernel.sh
@@ -27,6 +27,8 @@ if [[ $KERNEL_VERSION == "4.14" ]]; then
     sudo yum update -y kernel
 elif [[ $KERNEL_VERSION == "5.4" ]]; then
     sudo amazon-linux-extras install -y kernel-5.4
+elif [[ $KERNEL_VERSION == "5.10" ]]; then
+    sudo amazon-linux-extras install -y kernel-5.10
 else
     echo "$KERNEL_VERSION is not a valid kernel version"
     exit 1
