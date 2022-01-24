06edb03ae5bd585089a271994cc859f30e426ac1
Sylvain Rabot <sylvain@abstraction.fr>
2022-01-24 09:24:58 +0100

    Upgrade cilium to v1.11.1
    Signed-off-by: Sylvain Rabot <sylvain@abstraction.fr>

--- files/container_preload_images.sh
+++ files/container_preload_images.sh
@@ -15,6 +15,6 @@ sudo docker pull k8s.gcr.io/sig-storage/livenessprobe:v2.2.0
 sudo docker pull public.ecr.aws/aws-ec2/aws-node-termination-handler:v1.13.3
 sudo docker pull public.ecr.aws/aws-observability/aws-for-fluent-bit:2.21.4
 sudo docker pull quay.io/cilium/cilium:v1.10.6
-sudo docker pull quay.io/cilium/cilium:v1.11.0
+sudo docker pull quay.io/cilium/cilium:v1.11.1
 sudo docker pull quay.io/cilium/startup-script:62bfbe88c17778aad7bef9fa57ff9e2d4a9ba0d8
 sudo docker pull quay.io/prometheus/node-exporter:v1.3.1
