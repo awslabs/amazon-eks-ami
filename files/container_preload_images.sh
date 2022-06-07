a1b4124e3ed1a451295b86a9f9e63d8d3a38451d
Sylvain Rabot <sylvain.rabot@artex.io>
2022-06-07 15:42:05 +0200

    Upgrade docker images
    Signed-off-by: Sylvain Rabot <sylvain.rabot@artex.io>

--- files/container_preload_images.sh
+++ files/container_preload_images.sh
@@ -8,20 +8,16 @@ sudo systemctl start docker
 aws ecr get-login-password --region eu-central-1 | sudo docker login -u AWS --password-stdin 602401143452.dkr.ecr.eu-central-1.amazonaws.com 
 
 # kubectl get daemonset -o json --all-namespaces | jq -r '.items[].spec.template.spec.containers[].image' | sort
-sudo docker pull public.ecr.aws/eks-distro/coredns/coredns:v1.8.4-eks-1-21-7
-sudo docker pull public.ecr.aws/eks-distro/coredns/coredns:v1.8.7-eks-1-22-6
-sudo docker pull 602401143452.dkr.ecr.eu-central-1.amazonaws.com/eks/aws-ebs-csi-driver:v1.5.1
+sudo docker pull public.ecr.aws/eks-distro/coredns/coredns:v1.8.7-eks-1-22-7
 sudo docker pull 602401143452.dkr.ecr.eu-central-1.amazonaws.com/eks/aws-ebs-csi-driver:v1.5.3
+sudo docker pull public.ecr.aws/ebs-csi-driver/aws-ebs-csi-driver:v1.6.2
 sudo docker pull k8s.gcr.io/sig-storage/csi-node-driver-registrar:v2.1.0
-sudo docker pull k8s.gcr.io/sig-storage/csi-node-driver-registrar:v2.5.0
-sudo docker pull k8s.gcr.io/sig-storage/livenessprobe:v2.2.0
+sudo docker pull k8s.gcr.io/sig-storage/csi-node-driver-registrar:v2.5.1
+sudo docker pull k8s.gcr.io/sig-storage/livenessprobe:v2.4.0
 sudo docker pull k8s.gcr.io/sig-storage/livenessprobe:v2.6.0
-sudo docker pull public.ecr.aws/aws-ec2/aws-node-termination-handler:v1.13.3
-sudo docker pull public.ecr.aws/aws-ec2/aws-node-termination-handler:v1.16.1
-sudo docker pull public.ecr.aws/aws-observability/aws-for-fluent-bit:2.21.4
-sudo docker pull public.ecr.aws/aws-observability/aws-for-fluent-bit:2.23.3
-sudo docker pull quay.io/cilium/cilium:v1.11.2
-sudo docker pull quay.io/cilium/cilium:v1.11.4
+sudo docker pull public.ecr.aws/aws-ec2/aws-node-termination-handler:v1.16.5
+sudo docker pull public.ecr.aws/aws-observability/aws-for-fluent-bit:2.25.1
+sudo docker pull quay.io/cilium/cilium:v1.11.5
 sudo docker pull quay.io/cilium/startup-script:62bfbe88c17778aad7bef9fa57ff9e2d4a9ba0d8
 sudo docker pull quay.io/prometheus/node-exporter:v1.3.1
 
