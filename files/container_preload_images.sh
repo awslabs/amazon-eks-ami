f6ac79b5a34ca684a9ea37278d71edb55f0d3cb8
Sylvain Rabot <sylvain.rabot@artex.io>
2022-06-16 09:35:20 +0200

    Upgrade docker images
    Signed-off-by: Sylvain Rabot <sylvain.rabot@artex.io>

--- files/container_preload_images.sh
+++ files/container_preload_images.sh
@@ -18,6 +18,7 @@ sudo docker pull k8s.gcr.io/sig-storage/livenessprobe:v2.6.0
 sudo docker pull public.ecr.aws/aws-ec2/aws-node-termination-handler:v1.16.5
 sudo docker pull public.ecr.aws/aws-observability/aws-for-fluent-bit:2.25.1
 sudo docker pull quay.io/cilium/cilium:v1.11.5
+sudo docker pull quay.io/cilium/cilium:v1.11.6
 sudo docker pull quay.io/cilium/startup-script:62bfbe88c17778aad7bef9fa57ff9e2d4a9ba0d8
 sudo docker pull quay.io/prometheus/node-exporter:v1.3.1
 
