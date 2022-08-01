128ca8d69af6e8607f8c57fc9e5241edd7ba3e66
Carter McKinnon <mckdev@amazon.com>
2022-08-01 12:35:39 -0700

    Generate version-info.json during build

---
+++ scripts/generate-version-info.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# generates a JSON file containing version information for the software in this AMI
+
+if [ "$#" -ne 1 ]
+then
+  echo "usage: $0 OUTPUT_FILE"
+  exit 1
+fi
+
+OUTPUT_FILE="$1"
+
+# packages
+yum list installed --quiet | awk '{print $1, $2}' | jq -R 'inputs | split(" ") | {(.[0]):(.[1])}' | jq -s add  | jq '{packages:(.)}' > "$OUTPUT_FILE"
+
+# binaries
+echo $(jq ".binaries.kubelet = $(kubelet --version | awk '{print $2}')" $OUTPUT_FILE) > $OUTPUT_FILE
