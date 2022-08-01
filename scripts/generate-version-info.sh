7eb1f9f66caff9c27b7a2cbd8d38b96f4a7e11cc
Carter McKinnon <mckdev@amazon.com>
2022-08-01 13:09:44 -0700

    Fix jq syntax issue (missing quotes around string literal)

--- scripts/generate-version-info.sh
+++ scripts/generate-version-info.sh
@@ -2,6 +2,9 @@
 
 # generates a JSON file containing version information for the software in this AMI
 
+set -o errexit
+set -o pipefail
+
 if [ "$#" -ne 1 ]
 then
   echo "usage: $0 OUTPUT_FILE"
@@ -14,4 +17,4 @@ OUTPUT_FILE="$1"
 yum list installed --quiet | awk '{print $1, $2}' | jq -R 'inputs | split(" ") | {(.[0]):(.[1])}' | jq -s add  | jq '{packages:(.)}' > "$OUTPUT_FILE"
 
 # binaries
-echo $(jq ".binaries.kubelet = $(kubelet --version | awk '{print $2}')" $OUTPUT_FILE) > $OUTPUT_FILE
+echo $(jq ".binaries.kubelet = \"$(kubelet --version | awk '{print $2}')\"" $OUTPUT_FILE) > $OUTPUT_FILE
