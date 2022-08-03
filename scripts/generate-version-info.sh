d095c4e1b36b44b54024654f4e3c9f6e152015f2
Carter McKinnon <mckdev@amazon.com>
2022-08-03 15:32:40 -0700

    Use rpm, yum output isn't suitable for consumption by machines

--- scripts/generate-version-info.sh
+++ scripts/generate-version-info.sh
@@ -14,7 +14,7 @@ fi
 OUTPUT_FILE="$1"
 
 # packages
-yum list installed --quiet | awk '{print $1, $2}' | jq -R 'inputs | split(" ") | {(.[0]):(.[1])}' | jq -s add  | jq '{packages:(.)}' > "$OUTPUT_FILE"
+rpm --query --all --queryformat '\{"%{NAME}": "%{VERSION}-%{RELEASE}"\}\n' | jq --slurp --sort-keys 'add | {packages:(.)}' > "$OUTPUT_FILE"
 
 # binaries
 echo $(jq ".binaries.kubelet = \"$(kubelet --version | awk '{print $2}')\"" $OUTPUT_FILE) > $OUTPUT_FILE
