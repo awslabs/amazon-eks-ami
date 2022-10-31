ff27e2440b6a02d51ebcc5fec2ae42d315b31310
Saurav Agarwalla <saurav-agarwalla@users.noreply.github.com>
2022-10-31 14:15:30 -0400

    Pin Kernel 5.4 to 5.4.209-116.367 to prevent nodes from going into Unready (#1072)
    We're investigating issues with later Kernel versions which cause nodes to become Unready. Till those issues are resolved, pinning the Kernel to the last known good version.

--- scripts/upgrade_kernel.sh
+++ scripts/upgrade_kernel.sh
@@ -13,7 +13,12 @@ fi
 if [[ $KERNEL_VERSION == "4.14" ]]; then
     sudo yum update -y kernel
 elif [[ $KERNEL_VERSION == "5.4" ]]; then
-    sudo amazon-linux-extras install -y kernel-5.4
+    # Pinning Kernel to 5.4.209-116.367 since we're investigating issues with later Kernel versions which cause nodes to become Unready.
+    # sudo amazon-linux-extras install -y kernel-5.4
+    sudo amazon-linux-extras enable kernel-5.4=latest
+    sudo yum -y install kernel-5.4.209-116.367.amzn2
+    sudo yum install -y yum-plugin-versionlock
+    sudo yum versionlock kernel-5.4*
 elif [[ $KERNEL_VERSION == "5.10" ]]; then
     sudo amazon-linux-extras install -y kernel-5.10
 else
