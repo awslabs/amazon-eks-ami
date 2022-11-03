6014c4e6872a23f82ca295afa93b033207042876
Carter <mckdev@amazon.com>
2022-11-03 15:24:34 -0700

    Apply style rules to source files. (#1076)

--- test/cases/api-qps-k8s-1.21-below.sh
+++ test/cases/api-qps-k8s-1.21-below.sh
@@ -5,13 +5,13 @@ echo "--> Should use default API server QPS for K8s 1.21-"
 exit_code=0
 export KUBELET_VERSION=v1.21.0-eks-ba74326
 /etc/eks/bootstrap.sh \
-    --b64-cluster-ca dGVzdA== \
-    --apiserver-endpoint http://my-api-endpoint \
-    test || exit_code=$?
+  --b64-cluster-ca dGVzdA== \
+  --apiserver-endpoint http://my-api-endpoint \
+  test || exit_code=$?
 
 if [[ ${exit_code} -ne 0 ]]; then
-    echo "❌ Test Failed: expected a non-zero exit code but got '${exit_code}'"
-    exit 1
+  echo "❌ Test Failed: expected a non-zero exit code but got '${exit_code}'"
+  exit 1
 fi
 
 # values should not be set
@@ -21,11 +21,11 @@ expected_api_burst="null"
 actual_api_qps=$(jq -r '.kubeAPIQPS' < /etc/kubernetes/kubelet/kubelet-config.json)
 actual_api_burst=$(jq -r '.kubeAPIBurst' < /etc/kubernetes/kubelet/kubelet-config.json)
 if [[ ${actual_api_qps} != ${expected_api_qps} ]]; then
-    echo "❌ Test Failed: expected kubeAPIQPS = '${expected_api_qps}' but got '${actual_api_qps}'"
-    exit 1
+  echo "❌ Test Failed: expected kubeAPIQPS = '${expected_api_qps}' but got '${actual_api_qps}'"
+  exit 1
 fi
 
 if [[ ${actual_api_burst} != ${expected_api_burst} ]]; then
-    echo "❌ Test Failed: expected kubeAPIBurst = '${expected_api_burst}' but got '${actual_api_burst}'"
-    exit 1
+  echo "❌ Test Failed: expected kubeAPIBurst = '${expected_api_burst}' but got '${actual_api_burst}'"
+  exit 1
 fi
