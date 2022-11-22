b1308e8a91dc42c9b01d20392652a0459b24606d
Victor Pineda <vgonzla@amazon.com>
2022-11-22 14:01:32 -0800

    Short-circut sandbox image fetching

--- files/pull-sandbox-image.sh
+++ files/pull-sandbox-image.sh
@@ -2,4 +2,10 @@
 set -euo pipefail
 
 sandbox_image="$(awk -F'[ ="]+' '$1 == "sandbox_image" { print $2 }' /etc/containerd/config.toml)"
+
+### Short-circuit fetching sandbox image if its already present
+if [[ "$(sudo ctr --namespace k8s.io image ls | grep $sandbox_image)" != "" ]]; then
+  exit 0
+fi
+
 /etc/eks/containerd/pull-image.sh "${sandbox_image}"
