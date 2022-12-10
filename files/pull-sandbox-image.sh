f1bb10b4b161fc43eddad04359b1ab03530dcb2e
Carter <mckdev@amazon.com>
2022-12-09 16:13:55 -0800

    Handle indentation when parsing `sandbox_image` (#1119)

--- files/pull-sandbox-image.sh
+++ files/pull-sandbox-image.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-sandbox_image="$(awk -F'[ ="]+' '$1 == "sandbox_image" { print $2 }' /etc/containerd/config.toml)"
+source <(grep "sandbox_image" /etc/containerd/config.toml | tr -d ' ')
 
 ### Short-circuit fetching sandbox image if its already present
 if [[ "$(sudo ctr --namespace k8s.io image ls | grep $sandbox_image)" != "" ]]; then
