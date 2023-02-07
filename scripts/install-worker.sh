a6b8b3a57f3a75dbdc01051ec0c2360dc5394713
Sichao Wang <sichaow@amazon.com>
2023-02-07 14:27:42 -0800

    Version lock header

--- scripts/install-worker.sh
+++ scripts/install-worker.sh
@@ -73,8 +73,6 @@ sudo yum install -y \
 # Remove any old kernel versions. `--count=1` here means "only leave 1 kernel version installed"
 sudo package-cleanup --oldkernels --count=1 -y
 
-sudo yum versionlock kernel
-
 # Remove the ec2-net-utils package, if it's installed. This package interferes with the route setup on the instance.
 if yum list installed | grep ec2-net-utils; then sudo yum remove ec2-net-utils -y -q; fi
 
