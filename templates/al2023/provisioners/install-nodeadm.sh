4640e872aa4bff26828c65ad0d7bcbef5bc72cd5
Carter <mckdev@amazon.com>
2024-01-16 09:58:39 -0800

    Build nodeadm without Dockerfile (#1573)

--- templates/al2023/provisioners/install-nodeadm.sh
+++ templates/al2023/provisioners/install-nodeadm.sh
@@ -4,23 +4,18 @@ set -o pipefail
 set -o nounset
 set -o errexit
 
-# use containerd to build nodeadm containerized via kaniko + nerdctl
-# see: https://github.com/GoogleContainerTools/kaniko
-sudo systemctl enable --now containerd
+sudo systemctl start containerd
+
 sudo nerdctl run \
   --rm \
-  -v $PROJECT_DIR:/workspace \
-  $KANIKO_IMAGE \
-  --dockerfile /workspace/Dockerfile \
-  --single-snapshot \
-  --context dir:///workspace/ \
-  --no-push
-sudo systemctl disable containerd
+  --workdir /workdir \
+  --volume $PROJECT_DIR:/workdir \
+  public.ecr.aws/eks-distro-build-tooling/golang:1.21 \
+  make build
 
 # move the nodeadm binary into bin folder
 sudo chmod a+x $PROJECT_DIR/_bin/nodeadm
 sudo mv $PROJECT_DIR/_bin/nodeadm /usr/bin/
-sudo rm -rf $PROJECT_DIR/_bin
 
 # enable nodeadm bootstrap systemd units
 sudo systemctl enable nodeadm-config nodeadm-run
