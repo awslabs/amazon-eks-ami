8e9cddf12430a7979e8705979b2c68c06e92ed7c
Nick Baker <ndbaker1@outlook.com>
2024-01-29 19:33:41 -0800

    cleanup nerdctl after nodeadm containerized build (#1599)
    * cleanup nerdctl after nodeadm containerized build
    
    * use host network and only remove build and dangling imagest

--- templates/al2023/provisioners/install-nodeadm.sh
+++ templates/al2023/provisioners/install-nodeadm.sh
@@ -4,14 +4,23 @@ set -o pipefail
 set -o nounset
 set -o errexit
 
+BUILD_IMAGE=public.ecr.aws/eks-distro-build-tooling/golang:1.21
+
 sudo systemctl start containerd
 
 sudo nerdctl run \
   --rm \
+  --network host \
   --workdir /workdir \
   --volume $PROJECT_DIR:/workdir \
-  public.ecr.aws/eks-distro-build-tooling/golang:1.21 \
+  $BUILD_IMAGE \
   make build
+
+# cleanup build image and snapshots
+sudo nerdctl rmi \
+  --force \
+  $BUILD_IMAGE \
+  $(sudo nerdctl images -a | grep none | awk '{ print $3 }')
 
 # move the nodeadm binary into bin folder
 sudo chmod a+x $PROJECT_DIR/_bin/nodeadm
