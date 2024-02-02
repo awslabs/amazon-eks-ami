5fd22578e8c1b6330bcc122a5e7a3c30b2dd9957
Sichao Wang <sichaow@amazon.com>
2024-02-02 20:07:10 +0000

    use full string in e2e

--- nodeadm/test/e2e/cases/kubeconfig-outpost/run.sh
+++ nodeadm/test/e2e/cases/kubeconfig-outpost/run.sh
@@ -11,4 +11,5 @@ wait::dbus-ready
 
 mock::kubelet 1.28.0
 nodeadm init --skip run --config-source file://config.yaml
-assert::file-contains /etc/hosts 'example.com'
+assert::file-contains /etc/hosts $'127.0.0.1\tlocalhost'
+assert::file-contains /etc/hosts $'::1\tlocalhost'
