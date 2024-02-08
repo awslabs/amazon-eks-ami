2a0c18c87cc9791384bbcab9cf637eeafd26cc03
Nick Baker <nbakerd@amazon.com>
2024-02-08 22:23:48 +0000

    fix retry

--- nodeadm/internal/util/retry.go
+++ nodeadm/internal/util/retry.go
@@ -5,8 +5,8 @@ import "time"
 func RetryExponentialBackoff(attempts int, initial time.Duration, f func() error) error {
 	var err error
 	wait := initial
-	for i := 0; i < attempts; {
-		if err = f(); err != nil {
+	for i := 0; i < attempts; i++ {
+		if err = f(); err == nil {
 			return nil
 		}
 		time.Sleep(wait)
