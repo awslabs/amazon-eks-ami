579fd51dedccc1f5e76b9b234e0a90828abaf5dd
Carter McKinnon <mckdev@amazon.com>
2024-02-23 16:15:09 -0800

    Fix e2e test

--- nodeadm/test/e2e/cases/local-disks/run.sh
+++ nodeadm/test/e2e/cases/local-disks/run.sh
@@ -6,6 +6,10 @@ set -o pipefail
 
 source /helpers.sh
 
+mock::imds
+wait::dbus-ready
+mock::kubelet 1.29.0
+
 nodeadm init --skip run --config-source file://config.yaml
 
 assert::file-contains /etc/systemd/system/setup-local-disks.service.d/00-strategy.conf 'Environment=LOCAL_DISK_STRATEGY=raid0'
