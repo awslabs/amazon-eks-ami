c08195948d60dd0cdf085366754098afa7ff7e34
Paweł Mudlaff <pawel.mudlaff@fluenceenergy.com>
2024-06-05 14:59:57 +0200

    Update fluence script to validate if module was loaded and check if installation of kmod is necessary

--- templates/fluence/provisioners/load_kernel_modules.sh
+++ templates/fluence/provisioners/load_kernel_modules.sh
@@ -2,6 +2,8 @@
 
 if [[ "$ENABLE_FLUENCE_KERNEL_MODULES" == "true" ]]; then
   echo "Load kernel modules needed by fluence operations"
-  sudo yum install kmod
+  #sudo yum install kmod
   sudo modprobe nf_nat_ftp
+  echo "Check what kernel modules are loaded"
+  lsmod
 fi
