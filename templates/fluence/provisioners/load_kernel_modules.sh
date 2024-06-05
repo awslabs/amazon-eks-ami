5f30aa56f5e435af5af13812e7ccf509327e6bfe
Paweł Mudlaff <pawel.mudlaff@fluenceenergy.com>
2024-06-05 15:50:20 +0200

    update script to set loaded kernel modules

--- templates/fluence/provisioners/load_kernel_modules.sh
+++ templates/fluence/provisioners/load_kernel_modules.sh
@@ -2,6 +2,6 @@
 
 if [[ "$ENABLE_FLUENCE_KERNEL_MODULES" == "true" ]]; then
   echo "Set load kernel modules needed by fluence operations"
-  sudo tee -a /etc/modules-load.d/fluence.conf
+  sudo touch /etc/modules-load.d/fluence.conf
   sudo echo "nf_nat_ftp" >> /etc/modules-load.d/fluence.conf
 fi
