4c27e47c1b5151bba6037442b547ac476e228ae9
Paweł Mudlaff <pawel.mudlaff@fluenceenergy.com>
2024-06-05 14:29:48 +0200

    Update fluence module script

--- templates/fluence/provisioners/load_kernel_modules.sh
+++ templates/fluence/provisioners/load_kernel_modules.sh
@@ -2,6 +2,6 @@
 
 if [[ "$ENABLE_FLUENCE_KERNEL_MODULES" == "true" ]]; then
   echo "Load kernel modules needed by fluence operations"
-  yum install kmod
-  modprobe nf_nat_ftp
+  sudo yum install kmod
+  sudo modprobe nf_nat_ftp
 fi
