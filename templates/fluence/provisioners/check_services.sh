99d406b18e89486efff7b5f1d4ba6af9fccb1153
Paweł Mudlaff <pawel.mudlaff@fluenceenergy.com>
2024-06-19 13:43:29 +0200

    add command to start amazon ssm agent service

--- templates/fluence/provisioners/check_services.sh
+++ templates/fluence/provisioners/check_services.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo "Enable ssm-agent service"
-#sudo systemctl enable amazon-ssm-agent
-
 cat /usr/lib/systemd/system/amazon-ssm-agent.service
 
 echo "Check ssm-agent service status"
 sudo systemctl status amazon-ssm-agent --no-pager
+
+echo "Start ssm-agent service"
+sudo systemctl start amazon-ssm-agent --no-pager
 
 echo "Check what services are enabled"
 systemctl --type=service --no-pager --state=active
