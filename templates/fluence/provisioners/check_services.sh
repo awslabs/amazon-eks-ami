872ada5e42fafd4e0dacbb71ea3208f40c6e6943
Paweł Mudlaff <pawel.mudlaff@fluenceenergy.com>
2024-06-19 11:22:13 +0200

    enable amazon ssm agent service

--- templates/fluence/provisioners/check_services.sh
+++ templates/fluence/provisioners/check_services.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+echo "Enable ssm-agent service"
+sudo systemctl enable amazon-ssm-agent.service
+
 echo "Check what services are enabled"
 systemctl --type=service --no-pager --state=active
