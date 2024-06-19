b7e0443f52717ca5f2a14c45c2bbd2e04eb22f29
Paweł Mudlaff <pawel.mudlaff@fluenceenergy.com>
2024-06-19 13:57:30 +0200

    Check logs for failed start of ssm-agent

--- templates/fluence/provisioners/check_services.sh
+++ templates/fluence/provisioners/check_services.sh
@@ -8,5 +8,8 @@ sudo systemctl status amazon-ssm-agent --no-pager
 echo "Start ssm-agent service"
 sudo systemctl start amazon-ssm-agent --no-pager
 
+echo "Get ssm-agent logs"
+sudo journalctl -u amazon-ssm-agent --no-pager
+
 echo "Check what services are enabled"
 systemctl --type=service --no-pager --state=active
