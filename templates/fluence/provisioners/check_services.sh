058162d2ebf5a28d55c3d94f8db10fcb4b61e901
Paweł Mudlaff <pawel.mudlaff@fluenceenergy.com>
2024-06-21 11:47:41 +0200

    update journal logs command for amazon ssm agent

--- templates/fluence/provisioners/check_services.sh
+++ templates/fluence/provisioners/check_services.sh
@@ -5,11 +5,11 @@ cat /usr/lib/systemd/system/amazon-ssm-agent.service
 echo "Check ssm-agent service status"
 sudo systemctl status amazon-ssm-agent --no-pager
 
-echo "Start ssm-agent service"
-sudo systemctl start amazon-ssm-agent --no-pager
+# echo "Start ssm-agent service"
+# sudo systemctl start amazon-ssm-agent --no-pager
 
-echo "Get ssm-agent logs"
-sudo journalctl -u amazon-ssm-agent --no-pager --no-tail
+echo "Get ssm-agent logs - show error logs with additional info if available from last boot"
+sudo journalctl -u amazon-ssm-agent --no-pager -p 3 -xb
 
-echo "Check what services are enabled"
-systemctl --type=service --no-pager --state=active
+# echo "Check what services are enabled"
+# systemctl --type=service --no-pager --state=active
