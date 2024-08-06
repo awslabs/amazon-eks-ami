395942f3cadfd346a3a4a3da44b21b18e9cad869
Paweł Mudlaff <pawel.mudlaff@fluenceenergy.com>
2024-08-06 16:38:55 +0200

    Update config and script to validate kernel module that should be loaded

--- templates/fluence/provisioners/check_loaded_kernel_modules.sh
+++ templates/fluence/provisioners/check_loaded_kernel_modules.sh
@@ -4,4 +4,6 @@ if [[ "$ENABLE_FLUENCE_KERNEL_MODULES" == "true" ]]; then
   # call lsmod with sudo, otherwise there will be error that command is not found
   echo "Check what kernel modules are loaded"
   sudo lsmod
+  echo "Check nat ftp module information"
+  modinfo nf_nat_ftp
 fi
