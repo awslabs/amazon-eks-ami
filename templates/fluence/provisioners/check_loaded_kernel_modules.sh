ca38145b8bac0d185abb55c1f1246105fb822049
Paweł Mudlaff <pawel.mudlaff@fluenceenergy.com>
2024-08-07 14:38:15 +0200

    cleanup check kernel modules script and update circle ci config for release

--- templates/fluence/provisioners/check_loaded_kernel_modules.sh
+++ templates/fluence/provisioners/check_loaded_kernel_modules.sh
@@ -4,8 +4,4 @@ if [[ "$ENABLE_FLUENCE_KERNEL_MODULES" == "true" ]]; then
   # call lsmod with sudo, otherwise there will be error that command is not found
   echo "Check what kernel modules are loaded"
   lsmod
-  echo "Check nat ftp module information"
-  modinfo nf_nat_ftp
-  echo "Check module-load config"
-  ls -al /etc/modules-load.d
 fi
