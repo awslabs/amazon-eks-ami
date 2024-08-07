55b96c697161dd9c5660a278de4b272332f25b42
Paweł Mudlaff <pawel.mudlaff@fluenceenergy.com>
2024-08-07 12:28:19 +0200

    Update template to add restart stage and update check logs for added modules

--- templates/fluence/provisioners/check_loaded_kernel_modules.sh
+++ templates/fluence/provisioners/check_loaded_kernel_modules.sh
@@ -3,9 +3,11 @@
 if [[ "$ENABLE_FLUENCE_KERNEL_MODULES" == "true" ]]; then
   # call lsmod with sudo, otherwise there will be error that command is not found
   echo "Check what kernel modules are loaded"
-  sudo lsmod
+  lsmod
   echo "Check nat ftp module information"
   modinfo nf_nat_ftp
   echo "Check module-load config"
   ls -al /etc/modules-load.d
+  echo "Check content of fluence.conf
+  cat /etc/modules-load.d/fluence.conf
 fi
