a775165c3391b458c0d6170e46519038982c25f3
Kris Johnstone <kris.johnstone@fostermoore.com>
2024-09-06 15:29:43 +1000

    fix: install container-selinux when hardened is true

--- templates/al2023/provisioners/hardened-images.sh
+++ templates/al2023/provisioners/hardened-images.sh
@@ -8,4 +8,5 @@ if [[ "$HARDENED_IMAGE" == "true" ]]; then
   sudo chcon -t bin_t /usr/bin/nodeadm
   sudo chcon -t bin_t /usr/bin/kubelet
   sudo systemctl disable firewalld
+  sudo yum install container-selinux -y
 fi
