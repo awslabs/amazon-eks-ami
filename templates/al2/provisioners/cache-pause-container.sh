7f13765451fe4a88e89453ef536b5b5fef09dce6
Nick Baker <nbakerd@amazon.com>
2024-10-10 06:58:54 +0000

    rework cache script and handle docker for al2

--- templates/al2/provisioners/cache-pause-container.sh
+++ templates/al2/provisioners/cache-pause-container.sh
@@ -6,4 +6,18 @@ set -o pipefail
 
 AWS_DOMAIN=$(imds 'latest/meta-data/services/domain')
 ECR_URI="$(/etc/eks/get-ecr-uri.sh ${AWS_REGION} ${AWS_DOMAIN})"
-cache-pause-container "${ECR_URI}/eks/pause:3.5"
+
+TAG="localhost/kubernetes/pause:0.1.0"
+EXPORT_PATH=/etc/eks/pause.tar
+
+sudo systemctl start containerd
+cache-pause-container -i "${ECR_URI}/eks/pause:3.5" -t ${TAG} -e ${EXPORT_PATH}
+sudo systemctl stop containerd
+
+# we also need to import the image into docker, which is still default on 1.23
+# and supportted below 1.25.
+if vercmp ${KUBERNETES_VERSION} lt "1.25"; then
+  sudo systemctl start docker
+  sudo docker image import ${EXPORT_PATH} ${TAG}
+  sudo systemctl stop docker
+fi
--- templates/al2023/provisioners/cache-pause-container.sh
+++ templates/al2023/provisioners/cache-pause-container.sh
@@ -4,4 +4,6 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-cache-pause-container "$(nodeadm runtime ecr-uri)/eks/pause:3.5"
+sudo systemctl start containerd
+cache-pause-container -i "$(nodeadm runtime ecr-uri)/eks/pause:3.5"
+sudo systemctl stop containerd
