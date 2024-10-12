39795025b94eacffc9795049ef47a90dfe4c0d12
Nick Baker <nbakerd@amazon.com>
2024-10-12 00:00:52 +0000

    Scope merge transformer with internal types

--- nodeadm/internal/api/merge.go
+++ nodeadm/internal/api/merge.go
@@ -25,34 +25,17 @@ const (
 type nodeConfigTransformer struct{}
 
 func (t nodeConfigTransformer) Transformer(typ reflect.Type) func(dst, src reflect.Value) error {
-	if typ == reflect.TypeOf(ContainerdOptions{}) {
-		return func(dst, src reflect.Value) error {
-			return t.transformContainerdConfig(
-				dst.FieldByName(containerdConfigName),
-				src.FieldByName(containerdConfigName),
-			)
-		}
-	} else if typ == reflect.TypeOf(KubeletOptions{}) {
-		return func(dst, src reflect.Value) error {
-			t.transformKubeletFlags(
-				dst.FieldByName(kubeletFlagsName),
-				src.FieldByName(kubeletFlagsName),
-			)
-
-			if err := t.transformKubeletConfig(
-				dst.FieldByName(kubeletConfigName),
-				src.FieldByName(kubeletConfigName),
-			); err != nil {
-				return err
-			}
-
-			return nil
-		}
+	if typ == reflect.TypeOf(ContainerdConfig("")) {
+		return t.transformContainerdConfig
+	} else if typ == reflect.TypeOf(KubeletFlags{}) {
+		return t.transformKubeletFlags
+	} else if typ == reflect.TypeOf(InlineDocument{}) {
+		return t.transformInlineDocument
 	}
 	return nil
 }
 
-func (t nodeConfigTransformer) transformKubeletFlags(dst, src reflect.Value) {
+func (t nodeConfigTransformer) transformKubeletFlags(dst, src reflect.Value) error {
 	if dst.CanSet() {
 		// kubelet flags are parsed using https://github.com/spf13/pflag, where
 		// flag order determines precedence. For single-value flags this is
@@ -63,27 +46,6 @@ func (t nodeConfigTransformer) transformKubeletFlags(dst, src reflect.Value) {
 		// Based on this behavior, we choose to explicitly append slices for
 		// this field and no other slices.
 		dst.Set(reflect.AppendSlice(dst, src))
-	}
-}
-
-func (t nodeConfigTransformer) transformKubeletConfig(dst, src reflect.Value) error {
-	if dst.CanSet() {
-		if dst.Len() <= 0 {
-			// if the destination is empty just use the source data
-			dst.Set(src)
-		} else if src.Len() > 0 {
-			// kubelet config in an inline document here, so we explicitly
-			// perform a merge with dst and src data.
-			mergedMap, err := util.Merge(dst.Interface(), src.Interface(), json.Marshal, json.Unmarshal)
-			if err != nil {
-				return err
-			}
-			rawMap, err := toInlineDocument(mergedMap)
-			if err != nil {
-				return err
-			}
-			dst.Set(reflect.ValueOf(rawMap))
-		}
 	}
 	return nil
 }
@@ -107,6 +69,28 @@ func (t nodeConfigTransformer) transformContainerdConfig(dst, src reflect.Value)
 				return err
 			}
 			dst.SetString(string(config))
+		}
+	}
+	return nil
+}
+
+func (t nodeConfigTransformer) transformInlineDocument(dst, src reflect.Value) error {
+	if dst.CanSet() {
+		if dst.Len() <= 0 {
+			// if the destination is empty just use the source data
+			dst.Set(src)
+		} else if src.Len() > 0 {
+			// kubelet config in an inline document here, so we explicitly
+			// perform a merge with dst and src data.
+			mergedMap, err := util.Merge(dst.Interface(), src.Interface(), json.Marshal, json.Unmarshal)
+			if err != nil {
+				return err
+			}
+			rawMap, err := toInlineDocument(mergedMap)
+			if err != nil {
+				return err
+			}
+			dst.Set(reflect.ValueOf(rawMap))
 		}
 	}
 	return nil
