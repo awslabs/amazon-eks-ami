3ea41a27eb59faa3997ff6a151cc79a1535c2151
Nick Baker <nbakerd@amazon.com>
2024-10-15 21:16:25 +0000

    Remove AL2 bits

--- templates/al2023/provisioners/cache-pause-container.sh
+++ templates/al2023/provisioners/cache-pause-container.sh
@@ -5,5 +5,5 @@ set -o errexit
 set -o pipefail
 
 sudo systemctl start containerd
-cache-pause-container -i "$(nodeadm runtime ecr-uri)/eks/pause:3.5"
+cache-pause-container -i ${PAUSE_CONTAINER_IMAGE}
 sudo systemctl stop containerd
