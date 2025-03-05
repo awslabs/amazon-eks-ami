d14a8f9b9d3e56f46563f409b4d3c5d5fb43dd52
Mazen Selim <mazensel@amazon.com>
2025-03-05 05:56:59 +0000

    Use CredentialProviderConfig k8s type

--- nodeadm/go.mod
+++ nodeadm/go.mod
@@ -21,13 +21,27 @@ require (
 )
 
 require (
+	github.com/emicklei/go-restful/v3 v3.11.0 // indirect
 	github.com/fxamacker/cbor/v2 v2.7.0 // indirect
+	github.com/go-openapi/jsonpointer v0.21.0 // indirect
+	github.com/go-openapi/jsonreference v0.20.2 // indirect
+	github.com/go-openapi/swag v0.23.0 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/google/gnostic-models v0.6.8 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/stretchr/objx v0.5.2 // indirect
 	github.com/x448/float16 v0.8.4 // indirect
 	go.opentelemetry.io/otel v1.28.0 // indirect
 	go.opentelemetry.io/otel/trace v1.28.0 // indirect
+	golang.org/x/oauth2 v0.23.0 // indirect
+	golang.org/x/term v0.27.0 // indirect
+	golang.org/x/time v0.7.0 // indirect
+	k8s.io/client-go v0.32.2 // indirect
 	k8s.io/component-base v0.32.2 // indirect
+	k8s.io/kube-openapi v0.0.0-20241105132330-32ad38e42d3f // indirect
 )
 
 require dario.cat/mergo v1.0.1 // direct
--- nodeadm/internal/kubelet/image-credential-provider.go
+++ nodeadm/internal/kubelet/image-credential-provider.go
@@ -4,15 +4,22 @@ import (
 	"bytes"
 	_ "embed"
 	"fmt"
+	"log"
 	"os"
 	"path"
 	"path/filepath"
-	"text/template"
+	"time"
 
 	"github.com/awslabs/amazon-eks-ami/nodeadm/internal/api"
 	"github.com/awslabs/amazon-eks-ami/nodeadm/internal/util"
 	"go.uber.org/zap"
 	"golang.org/x/mod/semver"
+	k8smetav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	k8sruntime "k8s.io/apimachinery/pkg/runtime"
+	k8sjson "k8s.io/apimachinery/pkg/runtime/serializer/json"
+	k8sconfigv1 "k8s.io/kubelet/config/v1"
+	k8sconfigv1alpha1 "k8s.io/kubelet/config/v1alpha1"
+	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
 )
 
 const (
@@ -25,24 +32,7 @@ const (
 	ecrCredentialProviderBinPathEnvironmentName = "ECR_CREDENTIAL_PROVIDER_BIN_PATH"
 )
 
-var (
-	//go:embed image-credential-provider.json.tpl
-	imageCredentialProviderTemplateData string
-	imageCredentialProviderTemplate     = template.Must(template.New("image-credential-provider").Parse(imageCredentialProviderTemplateData))
-	imageCredentialProviderConfigPath   = path.Join(imageCredentialProviderRoot, imageCredentialProviderConfig)
-
-	matchImages = []string{"*.dkr.ecr.*.amazonaws.com",
-		"*.dkr-ecr.*.on.aws",
-		"*.dkr.ecr.*.amazonaws.com.cn",
-		"*.dkr-ecr.*.on.amazonwebservices.com.cn",
-		"*.dkr.ecr-fips.*.amazonaws.com",
-		"*.dkr-ecr-fips.*.on.aws",
-		"*.dkr.ecr.*.c2s.ic.gov",
-		"*.dkr.ecr.*.sc2s.sgov.gov",
-		"*.dkr.ecr.*.cloud.adc-e.uk",
-		"*.dkr.ecr.*.csp.hci.ic.gov",
-	}
-)
+var imageCredentialProviderConfigPath = path.Join(imageCredentialProviderRoot, imageCredentialProviderConfig)
 
 func (k *kubelet) writeImageCredentialProviderConfig(cfg *api.NodeConfig) error {
 	// fallback default for image credential provider binary if not overridden
@@ -55,7 +45,7 @@ func (k *kubelet) writeImageCredentialProviderConfig(cfg *api.NodeConfig) error 
 		return err
 	}
 
-	config, err := generateImageCredentialProviderConfig(cfg, ecrCredentialProviderBinPath)
+	config, err := generateImageCredentialProviderConfig(ecrCredentialProviderBinPath)
 	if err != nil {
 		return err
 	}
@@ -66,34 +56,70 @@ func (k *kubelet) writeImageCredentialProviderConfig(cfg *api.NodeConfig) error 
 	return util.WriteFileWithDir(imageCredentialProviderConfigPath, config, imageCredentialProviderPerm)
 }
 
-type imageCredentialProviderTemplateVars struct {
-	ConfigApiVersion   string
-	ProviderApiVersion string
-	EcrProviderName    string
-	MatchImages        []string
-}
-
-func generateImageCredentialProviderConfig(cfg *api.NodeConfig, ecrCredentialProviderBinPath string) ([]byte, error) {
-	templateVars := imageCredentialProviderTemplateVars{
-		EcrProviderName: filepath.Base(ecrCredentialProviderBinPath),
-		MatchImages:     matchImages,
+func generateImageCredentialProviderConfig(ecrCredentialProviderBinPath string) ([]byte, error) {
+	scheme := k8sruntime.NewScheme()
+	providerName := filepath.Base(ecrCredentialProviderBinPath)
+	defaultCacheDuration := &k8smetav1.Duration{Duration: 12 * time.Hour}
+	matchImages := []string{
+		"*.dkr.ecr.*.amazonaws.com",
+		"*.dkr-ecr.*.on.aws",
+		"*.dkr.ecr.*.amazonaws.com.cn",
+		"*.dkr-ecr.*.on.amazonwebservices.com.cn",
+		"*.dkr.ecr-fips.*.amazonaws.com",
+		"*.dkr-ecr-fips.*.on.aws",
+		"*.dkr.ecr.*.c2s.ic.gov",
+		"*.dkr.ecr.*.sc2s.sgov.gov",
+		"*.dkr.ecr.*.cloud.adc-e.uk",
+		"*.dkr.ecr.*.csp.hci.ic.gov",
 	}
 	kubeletVersion, err := GetKubeletVersion()
 	if err != nil {
 		return nil, err
 	}
+	var cfg k8sruntime.Object
 	if semver.Compare(kubeletVersion, "v1.27.0") < 0 {
-		templateVars.ConfigApiVersion = "kubelet.config.k8s.io/v1alpha1"
-		templateVars.ProviderApiVersion = "credentialprovider.kubelet.k8s.io/v1alpha1"
+		err = k8sconfigv1alpha1.AddToScheme(scheme)
+		if err != nil {
+			return nil, err
+		}
+		k8sconfigv1.AddToScheme(scheme)
+		cfg = &k8sconfigv1alpha1.CredentialProviderConfig{
+			Providers: []k8sconfigv1alpha1.CredentialProvider{
+				{
+					Name:                 providerName,
+					MatchImages:          matchImages,
+					DefaultCacheDuration: defaultCacheDuration,
+					APIVersion:           "credentialprovider.kubelet.k8s.io/v1alpha1",
+				},
+			},
+		}
 	} else {
-		templateVars.ConfigApiVersion = "kubelet.config.k8s.io/v1"
-		templateVars.ProviderApiVersion = "credentialprovider.kubelet.k8s.io/v1"
-		// ecr-credential-provider has support for public.ecr.aws in 1.27+
-		templateVars.MatchImages = append(templateVars.MatchImages, "public.ecr.aws")
+		err = k8sconfigv1.AddToScheme(scheme)
+		if err != nil {
+			return nil, err
+		}
+		cfg = &k8sconfigv1.CredentialProviderConfig{
+			Providers: []k8sconfigv1.CredentialProvider{
+				{
+					Name:                 providerName,
+					MatchImages:          append(matchImages, "public.ecr.aws"),
+					DefaultCacheDuration: defaultCacheDuration,
+					APIVersion:           "credentialprovider.kubelet.k8s.io/v1",
+				},
+			},
+		}
 	}
+	gvk, err := apiutil.GVKForObject(cfg, scheme)
+	if err != nil {
+		log.Println(err)
+	}
+	cfg.GetObjectKind().SetGroupVersionKind(gvk)
+	serializer := k8sjson.NewSerializerWithOptions(k8sjson.DefaultMetaFactory, scheme, scheme, k8sjson.SerializerOptions{Pretty: true})
+
 	var buf bytes.Buffer
-	if err := imageCredentialProviderTemplate.Execute(&buf, templateVars); err != nil {
-		return nil, err
+	err = serializer.Encode(cfg, &buf)
+	if err != nil {
+		log.Println(err)
 	}
 	return buf.Bytes(), nil
 }
