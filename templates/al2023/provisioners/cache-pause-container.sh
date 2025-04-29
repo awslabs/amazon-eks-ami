93c194d915d5f0a7f60270b24678ea4b5f9a9a15
vela <wwvela@amazon.com>
2025-04-28 17:40:28 -0700

    bring the cache sandbox image logic back for containerd 2.0

--- templates/al2023/provisioners/cache-pause-container.sh
+++ templates/al2023/provisioners/cache-pause-container.sh
@@ -4,10 +4,6 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-if [[ "$CONTAINERD_VERSION" == 2.0* ]]; then
-  exit 0
-fi
-
 sudo systemctl start containerd
 cache-pause-container -i ${PAUSE_CONTAINER_IMAGE}
 sudo systemctl stop containerd
