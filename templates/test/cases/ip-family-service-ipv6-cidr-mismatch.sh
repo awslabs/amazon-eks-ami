6e67c1ca5d545a8449c084eb69fd02732d29945e
guessi <guessi@gmail.com>
2025-05-24 00:40:54 +0800

    feat(bootstrap): accept service-ipv4-cidr as a flag

--- templates/test/cases/ip-family-service-ipv6-cidr-mismatch.sh
+++ templates/test/cases/ip-family-service-ipv6-cidr-mismatch.sh
@@ -8,7 +8,7 @@ exit_code=0
   --apiserver-endpoint http://my-api-endpoint \
   --ip-family ipv4 \
   --service-ipv6-cidr 192.168.0.1/24 \
-  test || exit_code=$?
+  ipv4-cluster || exit_code=$?
 
 if [[ ${exit_code} -eq 0 ]]; then
   echo "❌ Test Failed: expected a non-zero exit code but got '${exit_code}'"
