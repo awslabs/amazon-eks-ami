11526c883f5cf4bf5f64755c1a7e22e19e5ee366
vela <wwvela@amazon.com>
2025-05-27 14:50:49 -0700

    add containerd runtime spec test for both containerd v1 and v2 image

--- nodeadm/test/e2e/run.sh
+++ nodeadm/test/e2e/run.sh
@@ -15,44 +15,54 @@ fi
 
 # build image
 printf "🛠️ Building test infra image with containerd v1..."
-TEST_IMAGE=$(docker build -q -f test/e2e/infra/Dockerfile --build-arg CONTAINERD_VERSION=1.7.* .)
-echo "done! Test image with containerd v1: $TEST_IMAGE"
+CONTAINERD_V1_IMAGE=$(docker build -q -f test/e2e/infra/Dockerfile --build-arg CONTAINERD_VERSION=1.7.* .)
+echo "done! Test image with containerd v1: $CONTAINERD_V1_IMAGE"
 
 printf "🛠️ Building test infra image with containerd v2..."
-TEST_IMAGE2=$(docker build -q -f test/e2e/infra/Dockerfile --build-arg CONTAINERD_VERSION=2.0.5 .)
-echo "done! Test image with containerd v2: $TEST_IMAGE2"
+CONTAINERD_V2_IMAGE=$(docker build -q -f test/e2e/infra/Dockerfile --build-arg CONTAINERD_VERSION=2.0.5 .)
+echo "done! Test image with containerd v2: $CONTAINERD_V2_IMAGE"
 
-# Run tests
 FAILED="false"
 
-for CASE_DIR in $(ls -d test/e2e/cases/*); do
-  IMAGE=$TEST_IMAGE
-  CASE_NAME=$(basename $CASE_DIR)
-  if [[ "$CASE_NAME" == containerdv2-* ]]; then
-    IMAGE=$TEST_IMAGE2
-    printf "🧪 Testing $CASE_NAME with containerd v2 image..."
+function runTest() {
+  local case_name=$1
+  local image=$2
+  if [[ $image == $CONTAINERD_V1_IMAGE ]]; then
+    printf "🧪 Testing %s with containerd v1 image..." "$case_name"
   else
-    printf "🧪 Testing $CASE_NAME with containerd v1 image..."
+    printf "🧪 Testing %s with containerd v2 image..." "$case_name"
   fi
   CONTAINER_ID=$(docker run \
     -d \
     --rm \
     --privileged \
-    -v $NODEADM:/usr/local/bin/nodeadm \
-    -v $PWD/$CASE_DIR:/test-case \
-    $IMAGE)
+    -v "$NODEADM":/usr/local/bin/nodeadm \
+    -v "$PWD/$CASE_DIR":/test-case \
+    "$image")
   LOG_FILE=$(mktemp)
-  if docker exec $CONTAINER_ID bash -c "cd /test-case && ./run.sh" > $LOG_FILE 2>&1; then
+  if docker exec "$CONTAINER_ID" bash -c "cd /test-case && ./run.sh" > "$LOG_FILE" 2>&1; then
     echo "passed! ✅"
   else
     echo "failed! ❌"
-    cat $LOG_FILE
+    cat "$LOG_FILE"
     FAILED="true"
   fi
-  docker kill $CONTAINER_ID > /dev/null 2>&1
+  docker kill "$CONTAINER_ID" > /dev/null 2>&1
+}
+
+# Run tests
+for CASE_DIR in $(ls -d test/e2e/cases/*); do
+  CASE_NAME=$(basename "$CASE_DIR")
+  if [[ "$CASE_NAME" == containerdv2-* ]]; then
+    runTest "$CASE_NAME" "$CONTAINERD_V2_IMAGE"
+    continue
+  elif [[ "$CASE_NAME" == containerd-* ]]; then
+    runTest "$CASE_NAME" "$CONTAINERD_V2_IMAGE"
+  fi
+  runTest "$CASE_NAME" "$CONTAINERD_V1_IMAGE"
 done
 
-if [ "${FAILED}" = "true" ]; then
+if [ "$FAILED" = "true" ]; then
   echo "❌ Some tests failed!"
   exit 1
 else
