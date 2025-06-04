ecfb08620f420bb01a0f10b332ba8c4ed28d7e27
Carter McKinnon <mckdev@amazon.com>
2025-06-04 14:30:31 -0700

    feat(al2023): versionlock amazon-ec2-net-utils

--- templates/al2023/provisioners/install-worker.sh
+++ templates/al2023/provisioners/install-worker.sh
@@ -73,6 +73,9 @@ sudo dnf install -y \
 # needed by kubelet
 sudo dnf install -y iptables-nft
 
+# updating this package may trigger post-install hooks or config changes that undo what happens below
+sudo dnf versionlock amazon-ec2-net-utils
+
 # Mask udev triggers installed by amazon-ec2-net-utils package
 sudo touch /etc/udev/rules.d/99-vpc-policy-routes.rules
 
