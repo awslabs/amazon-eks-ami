18479f777632d8e7bd024990bb475e3c195fdfea
Nick Baker <nbakerd@amazon.com>
2025-06-04 08:58:37 +0000

    feat(nodeadm): pass nvidia gpu startup labels to kubelet

---
+++ nodeadm/test/e2e/cases/nvidia-gpu-labels/run.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+source /helpers.sh
+
+mock::aws
+mock::kubelet 1.32.0
+wait::dbus-ready
+
+nodeadm init --skip run --config-source file://config.yaml
+
+assert::file-not-contains /etc/eks/kubelet/environment '--node-labels=nvidia.com/gpu.present'
+assert::file-contains /etc/eks/kubelet/environment '--node-labels=foo=bar'
+
+# mock a pci device with the nvidia vendor-id.
+mock::pci-device "0x10de"
+
+nodeadm init --skip run --config-source file://config.yaml
+
+assert::file-contains /etc/eks/kubelet/environment '--node-labels=nvidia.com/gpu.present=true'
+assert::file-contains /etc/eks/kubelet/environment '--node-labels=foo=bar'
