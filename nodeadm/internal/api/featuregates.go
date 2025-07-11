a84efba9e4e4e08b0b7eb3154146cc7dde7d2ef6
Mazen Selim <mazensel@amazon.com>
2025-07-11 17:46:35 +0000

    feat(nodeadm): add feature gate to limit max pods value on memory

--- nodeadm/internal/api/featuregates.go
+++ nodeadm/internal/api/featuregates.go
@@ -14,6 +14,10 @@ var featureVerifiers = map[Feature]func(Feature, map[Feature]bool) bool{
 	// InstanceIdNodeNameGate controls whether to use instance ID as the node's name.
 	// By default, this feature is disabled, and the private DNS Name will be used.
 	InstanceIdNodeName: DefaultFalse,
+
+	// LimitMaxPods controls whether a ceiling should be applied to defualt or
+	// calculated max pods values based on the instance type's other resources.
+	LimitMaxPods: DefaultFalse,
 }
 
 func IsFeatureEnabled(feature Feature, featureGates map[Feature]bool) bool {
