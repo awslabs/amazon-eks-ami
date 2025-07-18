8b50618e366956ae4886b1a26c18e251ed46d109
Kris Johnstone <kris.johnstone@fostermoore.com>
2025-07-18 14:23:10 +1000

    feat: add start of nodeadm policy. install container-selinux as it has some policies that the compiled policies do not

--- templates/al2023/provisioners/hardened-images.sh
+++ templates/al2023/provisioners/hardened-images.sh
@@ -7,12 +7,14 @@ set -o errexit
 if [[ "$HARDENED_IMAGE" == "true" ]]; then
   sudo chcon -t bin_t /usr/bin/nodeadm && \
   sudo systemctl disable firewalld && \
-  sudo yum install make bzip2 selinux-policy-devel selinux-policy -y && \
+  sudo yum install make bzip2 selinux-policy-devel -y && \
   curl -L https://github.com/KrisJohnstone/container-selinux/archive/refs/heads/go-1.24.zip -o /tmp/container-selinux.zip && \
   cd /tmp/ && \
   unzip container-selinux.zip && \
   cd /tmp/container-selinux-go-1.24/ && \
   sed -i '/user_namespace/d' container.te && \
   sudo make install && \
-  sudo rm -rf container-selinux*
+  sudo yum remove make bzip2 -y && \
+  # There are policies provided by aws that dont appear to be upstream.
+  sudo yum install container-selinux -y
 fi
