d372c6432dfad800b6088a78b22f51cbf7d067ad
Kris Johnstone <kris.johnstone@fostermoore.com>
2025-07-18 16:57:59 +1000

    feat: add patch for go containers failing in selinux

--- templates/al2023/provisioners/hardened-images.sh
+++ templates/al2023/provisioners/hardened-images.sh
@@ -4,17 +4,34 @@ set -o pipefail
 set -o nounset
 set -o errexit
 
+################################################################################
+### Validate Required Arguments ################################################
+################################################################################
+validate_env_set() {
+  (
+    set +o nounset
+
+    if [ -z "${!1}" ]; then
+      echo "Packer variable '$1' was not set. Aborting"
+      exit 1
+    fi
+  )
+}
+
+validate_env_set HARDENED_IMAGE
+validate_env_set WORKING_DIR
+
+################################################################################
+### Install/Add Required Selinux Policies ######################################
+################################################################################
+
 if [[ "$HARDENED_IMAGE" == "true" ]]; then
   sudo chcon -t bin_t /usr/bin/nodeadm && \
   sudo systemctl disable firewalld && \
-  sudo yum install make bzip2 selinux-policy-devel -y && \
-  curl -L https://github.com/KrisJohnstone/container-selinux/archive/refs/heads/go-1.24.zip -o /tmp/container-selinux.zip && \
-  cd /tmp/ && \
-  unzip container-selinux.zip && \
-  cd /tmp/container-selinux-go-1.24/ && \
-  sed -i '/user_namespace/d' container.te && \
-  sudo make install && \
-  sudo yum remove make bzip2 -y && \
-  # There are policies provided by aws that dont appear to be upstream.
-  sudo yum install container-selinux -y
+  sudo yum install container-selinux -y && \
+  # Install Go 1.24 Patch that allows for read/write to socket.
+  cd "${WORKING_DIR}/tmp/selinux/go-1.24"
+  checkmodule -M -m -o go-1.24.mod go-1.24.te && \
+  semodule_package -o go-1.24.pp -m go-1.24.mod && \
+  sudo semodule -i go-1.24.pp
 fi
