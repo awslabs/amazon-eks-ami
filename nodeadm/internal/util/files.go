e1ec96ee1c7fa6b2dcbce1f9543f23e1c29525fd
Nick Baker <nbakerd@amazon.com>
2025-09-08 21:06:30 +0000

    move to manually unmanaged links

--- nodeadm/internal/util/files.go
+++ nodeadm/internal/util/files.go
@@ -10,7 +10,7 @@ import (
 // Wraps os.WriteFile to automatically create parent directories such that the
 // caller does not need to ensure the existence of the file's directory
 func WriteFileWithDir(filePath string, data []byte, perm fs.FileMode) error {
-	if err := os.MkdirAll(path.Dir(filePath), perm); err != nil {
+	if err := os.MkdirAll(path.Dir(filePath), perm|0111); err != nil {
 		return err
 	}
 	return os.WriteFile(filePath, data, perm)
