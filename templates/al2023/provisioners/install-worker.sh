7468d7f310617878025fd1c675baf832454dde80
husaiz <husaiz@amazon.com>
2025-09-30 16:44:08 -0700

    fix(al2023): Set correct SELinux context for binaries

--- templates/al2023/provisioners/install-worker.sh
+++ templates/al2023/provisioners/install-worker.sh
@@ -181,7 +181,7 @@ for binary in "${BINARIES[@]}"; do
   sudo sha256sum -c $binary.sha256
   sudo chmod +x $binary
   sudo chown root:root $binary
-  sudo mv $binary /usr/bin/
+  sudo mv --context $binary /usr/bin/
 done
 
 sudo rm ./*.sha256
