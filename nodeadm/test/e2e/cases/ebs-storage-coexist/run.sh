6b39f096bde2f283db8953dcd87657c8c03cb583
maitreya niranjan <mainira@amazon.com>
2026-07-10 18:55:57 +0000

    test: add e2e tests for EBS storage (containerd, kubelet, coexistence)

---
+++ nodeadm/test/e2e/cases/ebs-storage-coexist/run.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+source /helpers.sh
+
+mock::aws
+wait::dbus-ready
+mock::kubelet 1.35.0
+mock::setup-local-disks
+
+# Create a loopback device to simulate an EBS volume
+dd if=/dev/zero of=/tmp/ebs-disk.img bs=1M count=100
+LOOP_DEV=$(losetup -f --show /tmp/ebs-disk.img)
+
+# Update config to use the assigned loop device
+sed -i "s|/dev/loop0|${LOOP_DEV}|g" config.yaml
+
+nodeadm init --daemon="" --config-source file://config.yaml
+
+# Verify EBS mounted containerd via systemd
+assert::file-contains /etc/systemd/system/var-lib-containerd.mount 'Where=/var/lib/containerd'
+assert::file-contains /etc/systemd/system/var-lib-containerd.mount 'Type=ext4'
+systemctl is-active var-lib-containerd.mount
+
+# Verify setup-local-disks was still called for NVMe RAID
+assert::file-contains /var/log/setup-local-disks.log 'raid0'
---
+++ nodeadm/test/e2e/cases/ebs-storage-containerd/run.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+source /helpers.sh
+
+mock::aws
+wait::dbus-ready
+mock::kubelet 1.35.0
+
+# Create a loopback device to simulate an EBS volume
+dd if=/dev/zero of=/tmp/ebs-disk.img bs=1M count=100
+LOOP_DEV=$(losetup -f --show /tmp/ebs-disk.img)
+
+# Update config to use the assigned loop device
+sed -i "s|/dev/loop0|${LOOP_DEV}|g" config.yaml
+
+nodeadm init --daemon="" --config-source file://config.yaml
+
+# Verify the systemd mount unit was created and is active
+assert::file-contains /etc/systemd/system/var-lib-containerd.mount 'Where=/var/lib/containerd'
+assert::file-contains /etc/systemd/system/var-lib-containerd.mount 'Type=ext4'
+
+# Verify the mount is active
+systemctl is-active var-lib-containerd.mount
